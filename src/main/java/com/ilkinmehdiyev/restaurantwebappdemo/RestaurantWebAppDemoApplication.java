25cad530dfb15f78249003307e550275ad80976b
Ilkin Mehdiyev <mehdiyevilkin01@gmail.com>
2021-05-10 23:54:35 +0400

    initial commit

---
+++ src/main/java/com/ilkinmehdiyev/restaurantwebappdemo/RestaurantWebAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.restaurantwebappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWebAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantWebAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilkinmehdiyev/restaurantwebappdemo/RestaurantWebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.restaurantwebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantWebAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
