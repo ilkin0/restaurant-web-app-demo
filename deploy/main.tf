923502df4f0b0797924d8a63c38c4956a208f3b6
ilkin <mehdiyevilkin01@gmail.com>
2021-08-14 00:05:12 +0400

    WIP correcting AWS S3 connection problem

--- deploy/main.tf
+++ deploy/main.tf
@@ -6,12 +6,16 @@ terraform {
     region = "us-east-2"
     encrypt = true
     dynamodb_table = "restaurant-api-devops-tfstate-lock"
+//    profile = "ilkin.mehdiyev"
+//    shared_credentials_file = "~/.aws/config"
+//    token = "arn:aws:iam::009701377962:mfa/ilkin.mehdiyev"
   }
 }
 
 // provider for terraform
 provider "aws" {
   region = "us-east-2"
+  profile = "ilkin.mehdiyev"
 //  version = "~> 3.25.0"
 }
 
